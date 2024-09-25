93842b080ba84ff598f663e676b803ad8ae95399
mdefatimaperez <77554434+mdefatimaperez@users.noreply.github.com>
2024-09-25 16:07:22 -0300

    Add files via upload
    fix

--- site.js
+++ site.js
@@ -1,11 +1,37 @@
-
-let nombre = prompt("Cuál es tu nombre?").toUpperCase();
-while (nombre.length < 3) {
-  nombre = prompt(
-    "Mmmm, tu nombre no puede ser tan corto, ingresa al menos 3 letras",
-  ).toUpperCase();
+//EL SIGUIENTE CODIGO CORRESPONDE A : Profile dropdown
+const userMenu = document.getElementById("user-menu");
+const userMenuButton = document.getElementById("user-menu-button");
+// Función para mostrar el menú
+function showMenu() {
+  userMenu.classList.remove("hidden");
 }
-const span = document.getElementById("nombre");
-span.textContent = `Hola, ${nombre}`;
-const i = document.querySelector("i");
-i.setAttribute("class", "fa fa-ticket"); 
+// Función para ocultar el menú
+function hideMenu() {
+  userMenu.classList.add("hidden");
+}
+// Mostrar el menú al hacer clic en el botón
+userMenuButton.addEventListener("click", showMenu);
+// Ocultar el menú al hacer clic fuera del menú
+document.addEventListener("click", function (event) {
+  if (!userMenu.contains(event.target) && !userMenuButton.contains(event.target)) {
+    hideMenu();
+  }
+});
+
+
+// EL SIGUIENTE CODIGO CORRESPONDE AL MENU MOVIL
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenuContent = document.getElementById('mobile-menu-content');
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenuContent.classList.toggle('hidden');
+
+  // Actualizar el atributo aria-expanded del botón
+  mobileMenuButton.setAttribute('aria-expanded', mobileMenuContent.classList.contains('hidden') ? 'false' : 'true');
+
+  // Cambiar el ícono del botón
+  const openIcon = mobileMenuButton.querySelector('svg.block');
+  const closeIcon = mobileMenuButton.querySelector('svg.hidden');
+  openIcon.classList.toggle('hidden');
+  closeIcon.classList.toggle('hidden');
+});
